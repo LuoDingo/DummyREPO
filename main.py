67a9c99e3d36d8d19ca7e39c02c7e8001b8b9e1c
brian <bchou515@gmail.com>
2020-03-04 16:08:16 -0500

    brians changes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def print_hello():
     print("hello world")
     print("i made a change biotch")
-x = 5
+
 def math(x):
     return x**2
 
 if __name__ == "__main__":
     print_hello()
+    print(math(3))
